a9353b1d4e416223cb65f718f2ae16e794561c25
John Houston <jhouston@hashicorp.com>
2024-03-20 14:52:17 -0400

    add godocs to HCL configuration types

--- internal/generator/hcl_configuration.go
+++ internal/generator/hcl_configuration.go
@@ -10,24 +10,52 @@ type GeneratorConfig struct {
 
 // ResourceConfig configures code generation for a Terraform resource
 type ResourceConfig struct {
-	Name    string `hcl:"name,label"`
+	// Name is the terraform name of this resource
+	Name string `hcl:"name,label"`
+
+	// Package is the name of the Go package for the source files for this resource
 	Package string `hcl:"package"`
 
+	// OutputFilenamePrefix is a prefix to be added to all source files generated
+	// for this resource
 	OutputFilenamePrefix string `hcl:"output_filename_prefix"`
 
+	// APIVersion is the Kubernetes API version of the resource
 	APIVersion string `hcl:"api_version"`
-	Kind       string `hcl:"kind"`
 
+	// Kind is the Kubernetes kind of the resource
+	Kind string `hcl:"kind"`
+
+	// Description is a Markdown description for the resource
 	Description string `hcl:"description"`
 
-	IgnoreAttributes    []string `hcl:"ignore_attributes,optional"`
-	ComputedAttributes  []string `hcl:"computed_attributes,optional"`
+	// IgnoreAttributes is a list of attribute paths to omit from the resource
+	IgnoreAttributes []string `hcl:"ignore_attributes,optional"`
+
+	// FIXME not implemented yet
+	// RequiredAttributes is a list of attribute paths to mark as required in the schema
+	RequiredAttributes []string `hcl:"required_attributes,optional"`
+
+	// FIXME not implemented yet
+	// ComputedAttributes is a list of attribute paths to mark as computed in the schema
+	ComputedAttributes []string `hcl:"computed_attributes,optional"`
+
+	// FIXME not implemented yet
+	// SensitiveAttributes is a list of attribute paths to mark as sensitive in the schema
 	SensitiveAttributes []string `hcl:"sensitive_attributes,optional"`
 
+	// FIXME not implemented yet
+	// ImmutableAttributes is a list of attribute paths to mark as requiring a forced
+	// replacement if changed in the schema
+	ImmutableAttributes []string `hcl:"immutable_attributes,optional"`
+
+	// Generate controls generator specific options
 	Generate GenerateConfig `hcl:"generate,block"`
 
+	// OpenAPIConfig configures options for the OpenAPI to Framework IR generator
 	OpenAPIConfig TerraformPluginGenOpenAPIConfig `hcl:"openapi,block"`
 
+	// Disabled tells the generator to skip this configuration
 	Disabled bool `hcl:"disabled,optional"`
 }
 
@@ -39,9 +67,14 @@ type DataSourceConfig struct {
 // TerraformPluginGenOpenAPIConfig supplies configuration to tfplugingen-openapi
 // See: https://github.com/hashicorp/terraform-plugin-codegen-openapi
 type TerraformPluginGenOpenAPIConfig struct {
-	Filename   string `hcl:"filename"`
+	// Filename is the filename for the OpenAPI JSON specification
+	Filename string `hcl:"filename"`
+
+	// CreatePath is the POST path for the resource in the OpenAPI spec, e.g. /api/v1/namespaces/{namespace}/configmaps
 	CreatePath string `hcl:"create_path"`
-	ReadPath   string `hcl:"read_path"`
+
+	// ReadPath is the GET path for the resource in the OpenAPI spec, e.g. /api/v1/namespaces/{namespace}/configmaps/{name}
+	ReadPath string `hcl:"read_path"`
 }
 
 // GenerateConfig configures the options for what we should generate
